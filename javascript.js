a340b303f3b747932bcf0c5618a3d711067fdf61
jelyn <jelyn@gmail.com>
2024-02-26 14:36:10 +0800

    Add 2 features

---
+++ javascript.js
@@ -0,0 +1,20 @@
+body {
+  background-color: lightgray;
+  border-style: solid;
+  border-radius:5px;
+}
+
+h1 {
+  background-color:maroon;
+  text-align: center;
+  border-radius: 6px;
+  margin: 5px;
+}
+
+p {
+  margin:6px;
+  font-family: Arial;
+  font-size: 20px;
+  background-color:yellow;
+  border-radius: 6px;
+}
